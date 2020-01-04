3cc77a3bc9539d1f220a0f9f403c69a850555a25
Kongo <kongocode@gmail.com>
2020-01-04 09:59:16 +0100

    Replaced spaces to tabs and added editorconfig

--- src/components/PageEditor.tsx
+++ src/components/PageEditor.tsx
@@ -4,248 +4,253 @@ import Api from "services/Api";
 import ComponentsList from "shared/ComponentsList";
 import Grid from "shared/Grid/Grid";
 
+import 'scss/components/PageEditor.scss';
+
 export default class PageEditor extends React.Component<{}, {items: any[]}>
 {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        Api.deletePageEditorItems();
+		setTimeout(() => { debugger; },5000);
 
-        let items = Api.getPageEditorItems();
+		Api.deletePageEditorItems();
 
-        if(!items)
-        {
-            Api.savePageEditorItems([
-                {
-                    id: '1',
-                    location: ItemPosition.HEADER,
-                    index: 0,
+		let items = Api.getPageEditorItems();
 
-                    component: {
-                        name: 'ContentBox',
-                        props: {
-                            content: 'Header'
-                        }
-                    }
-                }, {
-                    id: '2',
-                    location: ItemPosition.LEFT,
-                    index: 0,
+		if(!items)
+		{
+			Api.savePageEditorItems([
+				{
+					id: '1',
+					location: ItemPosition.HEADER, 
+					index: 0,
 
-                    component: {
-                        name: 'ContentBox',
-                        props: {
-                            content: 'Page content box #1'
-                        }
-                    }
-                }, {
-                    id: '3',
-                    location: ItemPosition.LEFT,
-                    index: 1,
+					component: {
+						name: 'ContentBox',
+						props: {
+							content: 'Header'
+						}
+					}
+				}, {
+					id: '2',
+					location: ItemPosition.LEFT,
+					index: 0,
 
-                    component: {
-                        name: 'ContentBox',
-                        props: {
-                            content: 'Page content box #2'
-                        }
-                    }
-                }, {
-                    id: '4',
-                    location: ItemPosition.LEFT,
-                    index: 2,
+					component: {
+						name: 'ContentBox',
+						props: {
+							content: 'Page content box #1'
+						}
+					}
+				}, {
+					id: '3',
+					location: ItemPosition.LEFT,
+					index: 1,
 
-                    component: {
-                        name: 'ContentBox',
-                        props: {
-                            content: 'Page content box #3'
-                        }
-                    }
-                }, {
-                    id: '5',
-                    location: ItemPosition.RIGHT,
-                    content: '',
-                    index: 0,
+					component: {
+						name: 'ContentBox',
+						props: {
+							content: 'Page content box #2'
+						}
+					}
+				}, {
+					id: '4',
+					location: ItemPosition.LEFT,
+					index: 2,
 
-                    component: {
-                        name: 'ContentBox',
-                        props: {
-                            content: 'Right box #1'
-                        }
-                    }
-                }, {
-                    id: '6',
-                    location: ItemPosition.RIGHT,
-                    index: 1,
+					component: {
+						name: 'ContentBox',
+						props: {
+							content: 'Page content box #3'
+						}
+					}
+				}, {
+					id: '5',
+					location: ItemPosition.RIGHT,
+					content: '',
+					index: 0,
 
-                    component: {
-                        name: 'ContentBox',
-                        props: {
-                            content: 'Right box #2'
-                        }
-                    }
-                }
-            ]);
+					component: {
+						name: 'ContentBox',
+						props: {
+							content: 'Right box #1'
+						}
+					}
+				}, {
+					id: '6',
+					location: ItemPosition.RIGHT,
+					index: 1,
 
-            items = Api.getPageEditorItems();
-        }
+					component: {
+						name: 'ContentBox',
+						props: {
+							content: 'Right box #2'
+						}
+					}
+				}
+			]);
 
-        this.state = { items };
-    }
+			items = Api.getPageEditorItems();
+		}
 
-    componentDidUpdate(prevProps: Readonly<{}>, prevState: Readonly<{ items: any[] }>, snapshot?: any): void {
-        Api.savePageEditorItems(this.state.items);
-    }
-    
-    
-    render()
-    {
-        let gridProps = {
-            gridStyle: {width: '960px', margin: '0px auto'},
-            gridSize: {width: 12, height: 2},
-            columnSizes: Array(12).fill('auto'),
-            rowSizes: Array(2).fill('auto'),
-            areas: [
-                {x: 0, y: 0, width: 12, height: 1, name: ItemPosition.HEADER},
-                {x: 0, y: 1, width: 7, height: 1, name: ItemPosition.LEFT},
-                {x: 9, y: 1, width: 3, height: 1, name: ItemPosition.RIGHT}
-            ],
-            gridChildren: {
-                [ItemPosition.HEADER]:
-                    <Droppable droppableId={ItemPosition.HEADER}>
-                        {this.dropArea.bind(this, this.getItemsToRender(ItemPosition.HEADER))}
-                    </Droppable>,
+		this.state = { items };
+	}
 
-                [ItemPosition.LEFT]:
-                    <Droppable droppableId={ItemPosition.LEFT}>
-                        {this.dropArea.bind(this, this.getItemsToRender(ItemPosition.LEFT))}
-                    </Droppable>,
+	componentDidUpdate(prevProps: Readonly<{}>, prevState: Readonly<{ items: any[] }>, snapshot?: any): void {
+		Api.savePageEditorItems(this.state.items);
+	}
+	
+	
+	render()
+	{
+		let gridProps = {
+			gridStyle: {width: '960px', margin: '0px auto'},
+			gridSize: {width: 12, height: 2},
+			columnSizes: Array(12).fill('1fr'),
+			rowSizes: Array(2).fill('auto'),
+			areas: [
+				{x: 0, y: 0, width: 12, height: 1, name: ItemPosition.HEADER},
+				{x: 0, y: 1, width: 7, height: 1, name: ItemPosition.LEFT},
+				{x: 9, y: 1, width: 3, height: 1, name: ItemPosition.RIGHT}
+			],
+			gridChildren: {
+				[ItemPosition.HEADER]:
+					<Droppable droppableId={ItemPosition.HEADER}>
+						{this.DropArea.bind(this, this.getItemsToRender(ItemPosition.HEADER))}
+					</Droppable>,
 
-                [ItemPosition.RIGHT]:
-                    <Droppable droppableId={ItemPosition.RIGHT}>
-                        {this.dropArea.bind(this, this.getItemsToRender(ItemPosition.RIGHT))}
-                    </Droppable>,
-            }
-        };
+				[ItemPosition.LEFT]:
+					<Droppable droppableId={ItemPosition.LEFT}>
+						{this.DropArea.bind(this, this.getItemsToRender(ItemPosition.LEFT))}
+					</Droppable>,
 
-        return <div className="page-editor">
-            <DragDropContext onDragEnd={this.onDragEnd.bind(this)}>
-                <Grid {...gridProps} />
-            </DragDropContext>
-        </div>;
-    }
+				[ItemPosition.RIGHT]:
+					<Droppable droppableId={ItemPosition.RIGHT}>
+						{this.DropArea.bind(this, this.getItemsToRender(ItemPosition.RIGHT))}
+					</Droppable>,
+			}
+		};
 
-
-    private dropArea(items, provided, snapshot) {
-        return <div ref={provided.innerRef}>
-            {items}
-            {provided.placeholder}
-        </div>;
-    }
-
-    private getItemsToRender(type)
-    {
-        return this.getItemsOfType(type).map((item, index) => (
-            <Draggable
-                key={item.id}
-                draggableId={item.id}
-                index={index}>
-                {(provided, snapshot) => (
-                    <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={provided.draggableProps.style}>
-                        {this.renderComponent(item.component)}
-                    </div>
-                )}
-            </Draggable>
-        ));
-    }
+		return <div className="page-editor">
+			<DragDropContext onDragEnd={this.onDragEnd.bind(this)}>
+				<Grid {...gridProps} />
+			</DragDropContext>
+			
+		</div>;
+	}
 
 
-    private getItemsOfType(type)
-    {
-        return this.state.items.filter((v) => v.location === type).sort((a, b) => a.index - b.index);
-    }
+	private DropArea(items, provided, snapshot) {
+		return <div ref={provided.innerRef}>
+			{items}
+			{provided.placeholder}
+		</div>;
+	}
+
+	private getItemsToRender(type)
+	{
+		return this.getItemsOfType(type).map((item, index) => (
+			<Draggable
+				key={item.id}
+				draggableId={item.id}
+				index={index}>
+				{(provided, snapshot) => (
+					<div
+						ref={provided.innerRef}
+						{...provided.draggableProps}
+						{...provided.dragHandleProps}
+						style={provided.draggableProps.style}>
+						{this.renderComponent(item.component)}
+					</div>
+				)}
+			</Draggable>
+		));
+	}
 
 
-    private renderComponent(component: {name: string, props: object}) {
-        const {name, props} = component;
-
-        if (!(name in ComponentsList))
-            throw new Error(`Can't find component ${name}!`);
-
-        let ReactComponent = ComponentsList[name];
-
-        return <ReactComponent {...props} />;
-    }
+	private getItemsOfType(type)
+	{
+		return this.state.items.filter((v) => v.location === type).sort((a, b) => a.index - b.index);
+	}
 
 
+	private renderComponent(component: {name: string, props: object}) {
+		const {name, props} = component;
 
-    private onDragEnd( result ) {
-        const { source, destination } = result;
+		if (!(name in ComponentsList))
+			throw new Error(`Can't find component ${name}!`);
 
-        // dropped outside the list
-        if (!destination) {
-            return;
-        }
+		let ReactComponent = ComponentsList[name];
 
-        let items = this.move(
-            this.state.items,
-            result.draggableId,
-            source,
-            destination
-        );
-
-        this.setState({ items });
-    };
+		return <ReactComponent {...props} />;
+	}
 
 
-    /**
-     * Moves an item from one list to another list.
-     */
-    private move(
-        list: any[],
-        itemId,
-        source: {droppableId: string, index: number},
-        destination: {droppableId: string, index: number}
-    ) {
-        let movementDirection = destination.droppableId  == source.droppableId ? Math.sign(source.index - destination.index) : 1;
 
-        // Move item to new list:
-        for(let listItem of list) {
-            if(listItem.location != destination.droppableId) {
-                continue;
-            }
+	private onDragEnd( result ) {
+		const { source, destination } = result;
 
-            if(listItem.index >= destination.index) {
-                listItem.index += movementDirection;
-            }
-        }
+		// dropped outside the list
+		if (!destination) {
+			return;
+		}
 
-        // Move item itself:
-        let item = list.find((v) => v.id == itemId);
+		let items = this.move(
+			this.state.items,
+			result.draggableId,
+			source,
+			destination
+		);
 
-        item.index = destination.index;
-        item.location = destination.droppableId;
-
-        // Re-order old list:
-        let sourceListSorted = list.filter((v) => v.location === source.droppableId)
-                .sort((a, b) => a.index - b.index),
-            index = 0;
-
-        for(let listItem of sourceListSorted) {
-            listItem.index = index++;
-        }
+		this.setState({ items });
+	};
 
 
-        return list;
-    }
+	/**
+	 * Moves an item from one list to another list.
+	 */
+	private move(
+		list: any[],
+		itemId,
+		source: {droppableId: string, index: number},
+		destination: {droppableId: string, index: number}
+	) {
+		let movementDirection = destination.droppableId  == source.droppableId ? Math.sign(source.index - destination.index) : 1;
+
+		// Move item to new list:
+		for(let listItem of list) {
+			if(listItem.location != destination.droppableId) {
+				continue;
+			}
+
+			if(listItem.index >= destination.index) {
+				listItem.index += movementDirection;
+			}
+		}
+
+		// Move item itself:
+		let item = list.find((v) => v.id == itemId);
+
+		item.index = destination.index;
+		item.location = destination.droppableId;
+
+		// Re-order old list:
+		let sourceListSorted = list.filter((v) => v.location === source.droppableId)
+				.sort((a, b) => a.index - b.index),
+			index = 0;
+
+		for(let listItem of sourceListSorted) {
+			listItem.index = index++;
+		}
+
+
+		return list;
+	}
 }
 
 enum ItemPosition
 {
-    HEADER = 'header',
-    LEFT = 'left',
-    RIGHT = 'right',
+	HEADER = 'header',
+	LEFT = 'left',
+	RIGHT = 'right',
 }
--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,23 +1,23 @@
 export default class Api
 {
-    static savePageEditorItems(items)
-    {
-        window.localStorage.setItem('editor-items', JSON.stringify(items));
-    }
+	static savePageEditorItems(items)
+	{
+		window.localStorage.setItem('editor-items', JSON.stringify(items));
+	}
 
 
-    static getPageEditorItems()
-    {
-        let item = window.localStorage.getItem('editor-items');
+	static getPageEditorItems()
+	{
+		let item = window.localStorage.getItem('editor-items');
 
-        if(!item)
-            return null;
+		if(!item)
+			return null;
 
-        return JSON.parse(item);
-    }
+		return JSON.parse(item);
+	}
 
-    static deletePageEditorItems()
-    {
-        window.localStorage.removeItem('editor-items');
-    }
+	static deletePageEditorItems()
+	{
+		window.localStorage.removeItem('editor-items');
+	}
 }
